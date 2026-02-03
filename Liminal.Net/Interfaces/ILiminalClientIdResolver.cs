af7b781fb813a3004f1f5f0b11e4746b3fcd9d87
Demir <ahmeteage37@gmail.com>
2026-02-03 16:55:21 +0300

    Add transport and client-id resolver interfaces
    Introduce ILiminalTransport and ILiminalClientIdResolver interfaces to define an abstract transport layer and client ID resolution contract. ILiminalTransport includes connection lifecycle methods (StartServer/StartClient, Disconnect, Shutdown), send methods (SendReliable/SendUnreliable), events for reliable/unreliable message reception, and InitializeClientIdResolver; a DataReceivedHandler delegate and SERVER_ID constant were also added. ILiminalClientIdResolver exposes ResolveClientId and ReleaseId. Update the .csproj to include Core/ and Transport/ folders.

---
+++ Liminal.Net/Interfaces/ILiminalClientIdResolver.cs
@@ -0,0 +1,8 @@
+﻿namespace Liminal.Net.Interfaces
+{
+    public interface ILiminalClientIdResolver
+    {
+        public ushort ResolveClientId(Span<byte> payload);
+        public void ReleaseId(ushort clientId);
+    }
+}
---
+++ Liminal.Net/Interfaces/ILiminalTransport.cs
@@ -0,0 +1,42 @@
+﻿namespace Liminal.Net.Interfaces
+{
+    public delegate void DataReceivedHandler(ReadOnlySpan<byte> data, ushort clientId);
+
+    public interface ILiminalTransport
+    {
+        const ushort SERVER_ID = 0;
+        public bool IsConnected { get; }
+
+        #region Initialization
+        public void InitializeClientIdResolver(ILiminalClientIdResolver clientIdResolver);
+        #endregion
+
+        #region Connection
+        public void StartServer(string ip, int port);
+        public void StartClient(string ip, int port);
+        #endregion
+
+        #region Disconnection
+        /// <summary>
+        /// Disconnects the transport connection
+        /// Also can act as KickPlayer if clientId is not 0 and its server
+        /// </summary>
+        public void Disconnect(ushort clientId = SERVER_ID);
+
+        /// <summary>
+        /// Shuts down the transport 
+        /// </summary>
+        public void Shutdown();
+        #endregion
+
+        #region Sending
+        public void SendReliable(Span<byte> data, ushort clientId);
+        public void SendUnreliable(Span<byte> data, ushort clientId);
+        #endregion
+
+        #region Events
+        public event DataReceivedHandler OnMessageReceivedReliable;
+        public event DataReceivedHandler OnMessageReceivedUnreliable;
+        #endregion
+    }
+}
