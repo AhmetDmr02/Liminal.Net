c34799447c233c92f4e0f886ba2fdf22f5fd2e1b
Demir <ahmeteage37@gmail.com>
2026-02-03 15:59:35 +0300

    init commit

---
+++ Liminal.Net/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Liminal.Net
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            return;
+            
+        }
+    }
+}
