bef4c8856fc8c81463d3331911fefdea7860cb66
Demir <ahmeteage37@gmail.com>
2026-08-05 23:17:48 +0300

    Use atomic state and safe pinning in native buffer

--- Liminal.Net/Core/LiminalNativeBuffer.cs
+++ Liminal.Net/Core/LiminalNativeBuffer.cs
@@ -9,12 +9,17 @@ namespace Liminal.Net.Core
     {
         private byte* _ptr;
         private readonly int _length;
-        private int _disposed = 0;
 
-        internal bool IsDisposed => _disposed != 0;
+        private int _state;
+        private const int DisposedFlag = 1 << 31;
+
+        public bool IsDisposed => (Volatile.Read(ref _state) & DisposedFlag) != 0;
 
         public LiminalNativeBuffer(int length)
         {
+            if (length <= 0)
+                throw new ArgumentOutOfRangeException(nameof(length), "Length must be greater than zero.");
+
             _length = length;
             _ptr = (byte*)NativeMemory.Alloc((nuint)length);
         }
@@ -24,40 +29,91 @@ namespace Liminal.Net.Core
             Dispose(false);
         }
 
-        public Memory<byte> Memory => CreateMemory(_length);
-
         public override Span<byte> GetSpan()
         {
-            if (Volatile.Read(ref _disposed) != 0)
-                throw new ObjectDisposedException("LiminalNativeBuffer");
+            if (IsDisposed)
+                throw new ObjectDisposedException(nameof(LiminalNativeBuffer));
 
             return new Span<byte>(_ptr, _length);
         }
 
         public override MemoryHandle Pin(int elementIndex = 0)
         {
-            if ((uint)elementIndex >= (uint)_length) throw new ArgumentOutOfRangeException();
-            return new MemoryHandle(_ptr + elementIndex);
+            if ((uint)elementIndex > (uint)_length)
+                throw new ArgumentOutOfRangeException(nameof(elementIndex));
+
+            while (true)
+            {
+                int state = Volatile.Read(ref _state);
+
+                if ((state & DisposedFlag) != 0)
+                    throw new ObjectDisposedException(nameof(LiminalNativeBuffer));
+
+                int newState = state + 1;
+                if (Interlocked.CompareExchange(ref _state, newState, state) == state)
+                    break;
+            }
+
+            return new MemoryHandle(_ptr + elementIndex, pinnable: this);
         }
 
-        public override void Unpin() { }
+        public override void Unpin()
+        {
+            while (true)
+            {
+                int state = Volatile.Read(ref _state);
 
-        internal void ManualDispose() => Dispose(true);
+                if ((state & ~DisposedFlag) == 0)
+                    throw new InvalidOperationException("Unmatched Unpin. The buffer is not currently pinned.");
+
+                int newState = state - 1; 
+                if (Interlocked.CompareExchange(ref _state, newState, state) == state)
+                {
+                    if (newState == DisposedFlag)
+                    {
+                        FreeMemory();
+                    }
+                    break;
+                }
+            }
+        }
+
+        internal void ManualDispose() => ((IDisposable)this).Dispose();
 
         protected override void Dispose(bool disposing)
         {
-            if (Interlocked.Exchange(ref _disposed, 1) == 0)
+            while (true)
             {
-                if (_ptr != null)
-                {
-                    NativeMemory.Free(_ptr);
-                    _ptr = null;
-                }
+                int state = Volatile.Read(ref _state);
 
-                if (disposing)
+                if ((state & DisposedFlag) != 0)
+                    return; 
+
+                int newState = state | DisposedFlag;
+                if (Interlocked.CompareExchange(ref _state, newState, state) == state)
                 {
-                    GC.SuppressFinalize(this);
+                    if (newState == DisposedFlag)
+                    {
+                        FreeMemory();
+                    }
+
+                    if (disposing)
+                    {
+                        GC.SuppressFinalize(this);
+                    }
+                    break;
                 }
+            }
+        }
+
+        private void FreeMemory()
+        {
+            byte* ptr = _ptr;
+            _ptr = null;
+
+            if (ptr != null)
+            {
+                NativeMemory.Free(ptr);
             }
         }
     }
