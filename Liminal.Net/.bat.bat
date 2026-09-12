fe9b8196c2c7dc44f6844e47a9f133b18207d9b8
Demir <ahmeteage37@gmail.com>
2026-09-12 21:46:18 +0300

    merger bat

--- Liminal.Net/.bat.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -NoProfile -Command "Get-ChildItem -Recurse -Filter *.cs -File | Sort-Object FullName | ForEach-Object { \"===== $($_.FullName) =====\"; Get-Content -LiteralPath $_.FullName; \"\" } | Set-Content merged.txt"
-pause
